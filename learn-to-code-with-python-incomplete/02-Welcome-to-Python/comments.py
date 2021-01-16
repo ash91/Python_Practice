383f6b2f5681061c33719a822d21704c276987a1
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-01-16 12:28:04 +0530

    Update comments.py

--- learn-to-code-with-python-incomplete/02-Welcome-to-Python/comments.py
+++ learn-to-code-with-python-incomplete/02-Welcome-to-Python/comments.py
@@ -1,7 +1,8 @@
 # This is a comment
-print("Hello, World!")
+/*print("Hello, World!")
 print("Hello, World!")  # This is a comment
 # This is a comment
 # written in
 # more than just one line
 #print("Hello, World!")
+*/
