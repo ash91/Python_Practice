4fd2af2a9fcf0cbc2c43acc2d52b7ee0b53a6c1a
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-01-25 12:32:55 +0530

    Multiline comment

--- learn-to-code-with-python-incomplete/02-Welcome-to-Python/comments.py
+++ learn-to-code-with-python-incomplete/02-Welcome-to-Python/comments.py
@@ -1,6 +1,7 @@
-#This would be a comment in Python
+# This would be a comment in Python
 """ 
 This would be a multiline comment
 in Python that spans several lines and
 describes your code, your day, or anything you want it to
-""" 
+"""
+# Multiline comment
